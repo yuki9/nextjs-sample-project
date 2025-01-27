f27e1728c701e612610885f76144ae13cc4cfbc4
yuki09 <yuki.081009@gmail.com>
2025-01-27 17:50:23 +0900

    ニュースサイトにSSRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -12,6 +12,7 @@ type Props = {
     dk?: string;
   };
 };
+export const revalidate = 0;
 
 export default async function Page({ params, searchParams }: Props) {
   const data = await getNewsDetail(params.slug, {
